f7a9ce07dea07623acf045f17c561e9422d43319
Vince M. Treur <Vince.Treur@ing.com>
2021-08-04 09:52:01 +0200

    Re-used existing Consumable & ConsumingObserver

--- notes_feature/src/main/java/com/example/notesfeature/internal/notedetail/NoteDetailsNavigation.kt
+++ notes_feature/src/main/java/com/example/notesfeature/internal/notedetail/NoteDetailsNavigation.kt
@@ -2,7 +2,8 @@ package com.example.notesfeature.internal.notedetail
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+import com.example.notesfeature.internal.notelist.Consumable
+import com.example.notesfeature.internal.notelist.ConsumingObserver
 import com.example.notesfeature.internal.service.Note
 
 internal class NoteDetailsNavigation {
@@ -33,26 +34,4 @@ internal class NoteDetailsNavigator(
     private fun closeNote(note: Note) {
         fragment.dismiss()
     }
-}
-
-internal class Consumable<T>(
-    private val content: T
-) {
-    private var consumed = false
-
-    fun consume(block: (T) -> Unit) {
-        if (!consumed) {
-            consumed = true
-            block.invoke(content)
-        }
-    }
-}
-
-internal class ConsumingObserver<T>(private val action: (T) -> Unit) : Observer<Consumable<T>> {
-    override fun onChanged(t: Consumable<T>?) {
-        t?.consume {
-            action.invoke(it)
-        }
-    }
-
 }
