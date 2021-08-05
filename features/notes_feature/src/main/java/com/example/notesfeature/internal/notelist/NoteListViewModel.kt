ed7e1baff4f090a58a40826caf805d8afd43d015
Vince M. Treur <Vince.Treur@ing.com>
2021-08-05 14:23:39 +0200

    Switch to testable coroutines

--- features/notes_feature/src/main/java/com/example/notesfeature/internal/notelist/NoteListViewModel.kt
+++ features/notes_feature/src/main/java/com/example/notesfeature/internal/notelist/NoteListViewModel.kt
@@ -2,9 +2,10 @@ package com.example.notesfeature.internal.notelist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.example.backend.BackendCommunication
+import com.example.notesfeature.NotesDispatchers
 import com.example.notesfeature.internal.service.NoteService
+import kotlinx.coroutines.CoroutineScope
 
 internal class NoteListViewModel(backendCommunication: BackendCommunication) : ViewModel() {
 
@@ -15,7 +16,7 @@ internal class NoteListViewModel(backendCommunication: BackendCommunication) : V
         NoteListViewContainer(),
         NoteListNavigation(),
         NoteService(backendCommunication),
-        viewModelScope
+        CoroutineScope(NotesDispatchers.Main)
     )
 
     init {
